cabaaa19f7c0edfb81b6c4da2281bd483cb693ad
AshwinSakhare <asakhare@zdatainc.com>
2024-12-11 14:30:34 -0500

    update to include object_tracker script

--- sam2/build_sam.py
+++ sam2/build_sam.py
@@ -97,6 +97,45 @@ def build_sam2(
     return model
 
 
+def build_sam2_object_tracker(
+    num_objects,
+    config_file,
+    verbose=True,
+    ckpt_path=None,
+    device="cuda",
+    mode="eval",
+    hydra_overrides_extra=[],
+    apply_postprocessing=True,
+):
+    hydra_overrides = [
+        "++model._target_=sam2.sam2_object_tracker.SAM2ObjectTracker",
+    ]
+    if apply_postprocessing:
+        hydra_overrides_extra = hydra_overrides_extra.copy()
+        hydra_overrides_extra += [
+            # dynamically fall back to multi-mask if the single mask is not stable
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_via_stability=true",
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_delta=0.05",
+            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_thresh=0.98",
+            # the sigmoid mask logits on interacted frames with clicks in the memory encoder so that the encoded masks are exactly as what users see from clicking
+            "++model.binarize_mask_from_pts_for_mem_enc=true",
+            f"++model.num_objects={num_objects}",
+            f"++model.verbose={verbose}"
+        ]
+    hydra_overrides.extend(hydra_overrides_extra)
+
+    # Read config and init model
+    cfg = compose(config_name=config_file, overrides=hydra_overrides)
+    OmegaConf.resolve(cfg)
+    model = instantiate(cfg.model, _recursive_=True)
+    _load_checkpoint(model, ckpt_path)
+    model = model.to(device)
+    if mode == "eval":
+        model.eval()
+
+    return model
+
+
 def build_sam2_video_predictor(
     config_file,
     ckpt_path=None,
